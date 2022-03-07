30654aeacfe83aaad8b6802d8ebf737c7372db18
Lilian <58983575+Lilian-DL@users.noreply.github.com>
2022-03-07 18:39:10 +0100

    commit

---
+++ app.js
@@ -0,0 +1,32 @@
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+
+var studentRouter = require("./routes/students");
+
+var app = express();
+
+var mongoose = require("mongoose");
+
+var connexionStringLocal =
+  "mongodb+srv://Lilian:yScqkSzI13T3c9uh@iut.kfbdc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+var mongodb = process.env.MONGO_URI || connexionStringLocal;
+mongoose.connect(mongodb, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+mongoose.promise = global.Promise;
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/students", studentRouter);
+
+module.exports = app;
